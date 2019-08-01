5c40ba51090d85c4b7ca18c3933c27c1be50d560
jnm2 <me@jnm2.com>
2019-07-31 23:13:54 -0400

    Switch to Mono.Cecil

---
+++ src/EmbedDependencies.Tests/InsertEmbeddedAssemblyResolverTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Techsola.EmbedDependencies.Tests
+{
+    public static class InsertEmbeddedAssemblyResolverTests
+    {
+        private static MemoryStream ExecuteOnTestAssemblyStream()
+        {
+            var stream = new MemoryStream();
+
+            using (var file = File.OpenRead(typeof(InsertEmbeddedAssemblyResolverTests).Assembly.Location))
+                file.CopyTo(stream);
+
+            stream.Position = 0;
+
+            InsertEmbeddedAssemblyResolver.Execute(stream);
+
+            stream.Position = 0;
+
+            return stream;
+        }
+
+        [Test]
+        public static void Injected_module_initializer_should_be_valid()
+        {
+            using (var stream = ExecuteOnTestAssemblyStream())
+            {
+                var assembly = AppDomain.CurrentDomain.Load(stream.ToArray());
+
+                RuntimeHelpers.RunModuleConstructor(assembly.ManifestModule.ModuleHandle);
+            }
+        }
+    }
+}
--- src/EmbedDependencies/InsertEmbeddedAssemblyResolver.cs
+++ src/EmbedDependencies/InsertEmbeddedAssemblyResolver.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace Techsola.EmbedDependencies
 {
@@ -11,12 +15,113 @@ namespace Techsola.EmbedDependencies
 
         public override bool Execute()
         {
-            using (var stream = new FileStream(TargetAssembly.ItemSpec, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+            using var stream = new FileStream(TargetAssembly.ItemSpec, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+
+            Execute(stream);
+            return true;
+        }
+
+        public static void Execute(Stream stream)
+        {
+            var assemblyDefinition = AssemblyDefinition.ReadAssembly(stream, new ReaderParameters { ReadSymbols = false });
+
+            CreateModuleInitializer(assemblyDefinition.MainModule);
+
+            assemblyDefinition.Write(stream);
+        }
+
+        private static void CreateModuleInitializer(ModuleDefinition module)
+        {
+            var moduleType = module.GetType("<Module>");
+
+            var moduleInitializer = new MethodDefinition(".cctor", MethodAttributes.Static | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName, module.TypeSystem.Void);
+            var il = moduleInitializer.Body.GetILProcessor();
+
+            var parts = GetTargetFramework(module.Assembly)?.Split(',');
+            var frameworkName = parts?[0];
+            var version = parts is null ? null : Version.Parse(parts[1].Substring("Version=v".Length));
+
+            var appDomainAssemblyScope = module.TypeSystem.CoreLibrary;
+
+            if (frameworkName == ".NETCoreApp")
             {
-                Utils.InsertEmbeddedAssemblyResolver(stream);
+                if (version < new Version(2, 0))
+                    throw new NotSupportedException("Versions of .NET Core older than 2.0 are not supported.");
+
+                appDomainAssemblyScope = module.AssemblyReferences.Single(r => r.Name == "System.Runtime.Extensions");
+            }
+            else if (frameworkName == ".NETStandard")
+            {
+                if (version < new Version(2, 0))
+                    throw new NotSupportedException("Versions of .NET Standard older than 2.0 are not supported.");
             }
 
-            return true;
+            GenerateAppDomainModuleInitializerIL(module, moduleType, il, appDomainAssemblyScope);
+
+            moduleType.Methods.Add(moduleInitializer);
+        }
+
+        private static string GetTargetFramework(AssemblyDefinition assembly)
+        {
+            var targetFrameworkAttribute = assembly.CustomAttributes.SingleOrDefault(a => a.AttributeType.FullName == "System.Runtime.Versioning.TargetFrameworkAttribute");
+
+            return (string)targetFrameworkAttribute?.ConstructorArguments.First().Value;
+        }
+
+        private static void GenerateAppDomainModuleInitializerIL(ModuleDefinition module, TypeDefinition moduleType, ILProcessor il, IMetadataScope appDomainAssemblyScope)
+        {
+            var appDomainType = module.ImportReference(new TypeReference(
+                "System", "AppDomain", module: null, appDomainAssemblyScope, valueType: false));
+
+            var resolveEventHandlerType = module.ImportReference(new TypeReference(
+                "System", "ResolveEventHandler", module: null, appDomainAssemblyScope, valueType: false));
+
+            var assemblyResolveHandler = CreateAppDomainAssemblyResolveHandler(module);
+            moduleType.Methods.Add(assemblyResolveHandler);
+
+            il.Emit(OpCodes.Call, new MethodReference(
+                "get_CurrentDomain",
+                returnType: appDomainType,
+                declaringType: appDomainType));
+
+            il.Emit(OpCodes.Ldnull);
+            il.Emit(OpCodes.Ldftn, assemblyResolveHandler);
+            il.Emit(OpCodes.Newobj, new MethodReference(
+                ".ctor",
+                returnType: module.TypeSystem.Void,
+                declaringType: resolveEventHandlerType)
+            {
+                HasThis = true,
+                Parameters =
+                {
+                    new ParameterDefinition(module.TypeSystem.Object),
+                    new ParameterDefinition(module.TypeSystem.IntPtr)
+                }
+            });
+
+            il.Emit(OpCodes.Callvirt, new MethodReference(
+                "add_AssemblyResolve",
+                returnType: module.TypeSystem.Void,
+                declaringType: appDomainType)
+            {
+                HasThis = true,
+                Parameters = { new ParameterDefinition(resolveEventHandlerType) }
+            });
+
+            il.Emit(OpCodes.Ret);
+        }
+
+        private static MethodDefinition CreateAppDomainAssemblyResolveHandler(ModuleDefinition module)
+        {
+            var assemblyType = new TypeReference("System.Reflection", "Assembly", module, scope: null, valueType: false);
+
+            var handler = new MethodDefinition("OnAssemblyResolve", MethodAttributes.Static, assemblyType);
+            var il = handler.Body.GetILProcessor();
+
+            il.Emit(OpCodes.Ldnull);
+            il.Emit(OpCodes.Ret);
+
+            return handler;
         }
     }
 }
